203edb0d559a79fc3f042c38da682cf5f3bf0c99
copperstick6 <copperstick6@gmail.com>
2017-10-31 18:21:46 -0500

    added react website and databases

--- server.py
+++ server.py
@@ -1,7 +1,21 @@
 import sys
-
+import database
 from flask import Flask, jsonify, request, redirect, render_template
 import requests
 import json
 import flask
 app = Flask(__name__)
+
+
+@app.route('/getMemeName', methods=['POST'])
+def getName():
+	return json.dumps(database.get_memes_by_name(str(request.form['name'])))
+
+@app.route('/sendNewMeme', methods=['POST'])
+def sendNewMeme():
+	database.insert_data(str(request.form['name']), str(request.form['description']))
+
+@app.route('/getAllMemes', methods=['GET'])
+def getAll():
+	return json.dumps(database.get_memes())
+app.run()
